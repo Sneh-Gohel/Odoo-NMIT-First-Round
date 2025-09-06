45b920dfc9f29ac9c4e70801eafa53724b9ac3d3
Rajgohel0312 <rajgohel2018@gmail.com>
2025-09-06 11:02:11 +0530

    Merged Frontend Branch

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import About from "../../components/About/About";
-=======
-// import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import Hero from '../../components/Hero/Hero'
->>>>>>> 051af2eef1742c3385c19e0aa4fc9bea62de8fdd
+import Hero from "../../components/Hero/Hero";
 
 const LandingPage = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Navbar />
+      <Hero />
       <About />
-=======
-        <Navbar />
-        <Hero/>
-        <h1>Hello</h1>
->>>>>>> 051af2eef1742c3385c19e0aa4fc9bea62de8fdd
     </div>
   );
 };
